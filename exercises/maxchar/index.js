71a0985e9fdfd472cd4e385e69c7d1bbf0f3a65b
Hiep Nguyen <hiep.nguyen@solislab.com>
2020-01-25 22:27:40 +0700

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charsMap= {}
+    let max = 0
+    let maxChar = ''
+    for (let char of str) {
+        charsMap[char] = charsMap[char] + 1 || 1
+    }
+    for (let char in charsMap) {
+        if (charsMap[char] > max) {
+            max = charsMap[char]
+            maxChar = char
+        }
+    }
+    return maxChar
+}
 
 module.exports = maxChar;
