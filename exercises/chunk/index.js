717c2c16b4b99952cb4732976fb40461f9cf922d
Hiep Nguyen <hiep.nguyen@solislab.com>
2020-02-09 11:23:29 +0700

    Chunk Array with while loop and slice prototype

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -10,15 +10,25 @@
 
 function chunk(array, size) {
     const chunked = [];
-    for (let element of array) {
-        const last = chunked[chunked.length - 1];
-        if (!last || last.length === size) {
-            chunked.push([element])
-        } else {
-            last.push(element)
-        }
+    let index = 0;
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
     }
     return chunked;
 }
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//     const chunked = [];
+//     for (let element of array) {
+//         const last = chunked[chunked.length - 1];
+//         if (!last || last.length === size) {
+//             chunked.push([element])
+//         } else {
+//             last.push(element)
+//         }
+//     }
+//     return chunked;
+// }
