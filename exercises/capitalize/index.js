f28314cf519a5264b681780d6ec51de10e5d7778
Hiep Nguyen <hiep.nguyen@solislab.com>
2020-02-11 21:53:44 +0700

    Capitalize with if else

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,11 +8,23 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-    const words = [];
-    for (let word of str.split(' ')) {
-        words.push(word[0].toUpperCase() + word.slice(1));
+    let words = str[0].toUpperCase();
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] === ' ') {
+            words += str[i].toUpperCase();
+        } else {
+            words += str[i];
+        }
     }
-    return words.join(' ');
+    return words;
 }
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//     const words = [];
+//     for (let word of str.split(' ')) {
+//         words.push(word[0].toUpperCase() + word.slice(1));
+//     }
+//     return words.join(' ');
+// }
